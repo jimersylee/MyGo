cacaa2896f7fca310eb3718398f7883120edf786
jimersylee <jimersylee@gmail.com>
2017-09-06 11:51:47 +0800

    使用mysql数据库

--- 3.access-db/README.md
+++ 3.access-db/README.md
@@ -1,1 +1,32 @@
 #访问数据库
+go没有内置的驱动支持任何数据库,但是Go定义了database/sql接口
+
+
+##数据库驱动类,定义一些接口
+
+- driver.Driver
+- driver.Conn
+- driver.Stmt
+- driver.Execer
+- driver.Result
+- driver.Rows
+
+
+##使用mysql数据库
+通过上面的代码我们可以看出，Go操作Mysql数据库是很方便的。
+
+关键的几个函数我解释一下：
+
+sql.Open()函数用来打开一个注册过的数据库驱动，go-sql-driver中注册了mysql这个数据库驱动，第二个参数是DSN(Data Source Name)，它是go-sql-driver定义的一些数据库链接和配置信息。它支持如下格式：
+
+user@unix(/path/to/socket)/dbname?charset=utf8
+user:password@tcp(localhost:5555)/dbname?charset=utf8
+user:password@/dbname
+user:password@tcp([de:ad:be:ef::ca:fe]:80)/dbname
+db.Prepare()函数用来返回准备要执行的sql操作，然后返回准备完毕的执行状态。
+
+db.Query()函数用来直接执行Sql返回Rows结果。
+
+stmt.Exec()函数用来执行stmt准备好的SQL语句
+
+我们可以看到我们传入的参数都是=?对应的数据，这样做的方式可以一定程度上防止SQL注入。
---
+++ 3.access-db/mysql-example/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:@/go?charset=utf8")
+	checkErr(err)
+	//插入数据
+	stmt, err := db.Prepare("insert userinfo set username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("jimersylee", "dev", "2017-09-05")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+
+	//更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+	res, err = stmt.Exec("nameUpdate", id)
+	checkErr(err)
+	affect,err:=res.RowsAffected()
+	checkErr(err)
+	fmt.Println(affect)
+
+
+	//查询数据
+	rows,err:=db.Query("select * from userinfo")
+	checkErr(err)
+
+	for rows.Next(){
+		var uid int
+		var username string
+		var department string
+		var created string
+		err=rows.Scan(&uid,&username,&department,&created)
+		fmt.Println(uid,username,department,created)
+	}
+
+	//删除数据
+	stmt, err=db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+	res,err=stmt.Exec(id)
+	checkErr(err)
+
+	affect,err=res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+
+}
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
